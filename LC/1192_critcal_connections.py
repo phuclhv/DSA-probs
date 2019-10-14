62ec78e561efa5ee2f5f97555d62cab9437e6cea
Phuc-HomeUbuntu <lephuc66@gmail.com>
2019-10-14 16:29:56 -0700

    Add optimal solutions

--- LC/1192_critcal_connections.py
+++ LC/1192_critcal_connections.py
@@ -21,3 +21,41 @@ class Solution:
                     break
             connections.insert(i,remove_connection)
         return res
+
+'''
+Add 
+'''
+from collections import defaultdict
+
+class Solution2(object):
+    def criticalConnections(self, n, connections):
+        
+        graph = defaultdict(list)
+        
+        for connection in connections:
+            graph[connection[0]].append(connection[1])
+            graph[connection[1]].append(connection[0])
+        
+        disc = [None for _ in range(n)]
+        low = [None for _ in range(n)]
+        res = []
+        self.cur = 0
+        
+        def dfs(node, parent):
+            if disc[node] is None:
+                disc[node] = self.cur
+                low[node] = self.cur
+                self.cur += 1
+                for n in graph[node]:
+                    dfs(n, node)
+                for n in graph[node]:
+                    if low[n] < low[node] and n!=parent:
+                        low[node] = low[n]
+        
+        dfs(0, None)
+        
+        for connection in connections:
+            if low[connection[0]] > disc[connection[1]] or low[connection[1]] > disc[connection[0]]:
+                res.append(connection)
+        
+        return res    
